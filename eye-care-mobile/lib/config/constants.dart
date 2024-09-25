2720913beb437d745f2e53710e80066de5da71e0
Karyll <karyllrvr@gmail.com>
2024-09-26 02:40:56 +0800

    Initial commit with web, server, and mobile folders

---
+++ eye-care-mobile/lib/config/constants.dart
@@ -0,0 +1,8 @@
+class Constants {
+  // static const String eyecareApiUrl = 'http://localhost:8000/api';
+  static const String eyecareApiUrl =
+      'https://eye-care-server.onrender.com/api';
+}
+
+// http://localhost:8000/api
+// https://eye-care-server.onrender.com/api
---
+++ eye-care-server/src/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const compression = require("compression");
+const cors = require("cors");
+const { config } = require("dotenv");
+const mongoose = require("mongoose");
+const routers = require("./routers/index.js");
+
+config();
+const { PORT, MONGODB_URI, MONGODB_NAME } = process.env;
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use("/api", routers());
+
+mongoose.Promise = Promise;
+mongoose
+  .connect(MONGODB_URI, { dbName: MONGODB_NAME })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => console.error("Error connecting to MongoDB:", error));
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log("Server running on http://localhost:" + PORT);
+});
